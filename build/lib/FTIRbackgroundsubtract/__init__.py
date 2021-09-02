35472cdc071a4284b5e43f0035a73ec0537dc72b
Kayla Iacovino <kaylaiacovino@gmail.com>
2021-09-02 17:05:54 -0500

    First version (1.2.0) to go onto pip

---
+++ build/lib/FTIRbackgroundsubtract/__init__.py
@@ -0,0 +1,12 @@
+"""
+ftir-background-subtract
+
+A GUI tool for drawing polynomial backgrounds on FTIR spectra and subtracting
+them to get total absorbance (by height) of a desired peak.
+"""
+
+__version__ = "1.2.0"
+__author__ = 'Kayla Iacovino and Nial Peters'
+
+# ----------------- IMPORTS ----------------- #
+import FTIRbackgroundsubtract.core
